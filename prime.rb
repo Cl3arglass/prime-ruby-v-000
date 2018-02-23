f5655809599179dcf0ebe0b773435a70d5049ed2
Holden Altaffer <haltaffer@gmail.com>
2018-02-23 21:43:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@
 def prime?(num)
   if num == 2
     true
-  elsif num > 1 && num % num == 0 && num % 1 == 0 && (2 .. (num - 1)).to_a.any?{|number| num % number != 0}
+  elsif num > 1 && num % num == 0 && num % 1 == 0 && !(2 .. (num - 1)).to_a.any?{|number| num % number == 0}
      true
   else
     false
