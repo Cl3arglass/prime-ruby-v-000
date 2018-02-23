7c5820325b57c022f9a180ae870c225a1a1d3407
Holden Altaffer <haltaffer@gmail.com>
2018-02-23 21:41:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,9 @@
 # num > 0 && num % num == 0 && num % 1 == 0 && num % [2..num - 1] != 0
 # [2..num]
 def prime?(num)
-  if num == 1 || num == 2
+  if num == 2
     true
-  elsif num > 0 && num % num == 0 && num % 1 == 0 && (2 .. (num - 1)).to_a.any?{|number| num % number != 0}
+  elsif num > 1 && num % num == 0 && num % 1 == 0 && (2 .. (num - 1)).to_a.any?{|number| num % number != 0}
      true
   else
     false
